28a96f18db876f156fde5ff96c965c63abe37a50
digitalight <mikeglover06@gmail.com>
2020-04-11 17:04:22 +0100

    2020 version

--- sketch.js
+++ sketch.js
@@ -1,72 +1,81 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-
-// Snowfall
-// Edited Video: https://youtu.be/cl-mHFCGzYk
-
-let snow = [];
-let gravity;
-
-let zOff = 0;
-
-
-let spritesheet;
-let textures = [];
-
-function preload() {
-  spritesheet = loadImage('flakes32.png');
-}
+let particles = [];
+const particleCount = 100;
+const maxDistance = 100;
+const maxConnections = 2;
+const minSpeed = 0.05;
+const maxSpeed = 0.3;
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  gravity = createVector(0, 0.3);
-  for (let x = 0; x < spritesheet.width; x += 32) {
-    for (let y = 0; y < spritesheet.height; y += 32) {
-      let img = spritesheet.get(x, y, 32, 32);
-      image(img, x, y);
-      textures.push(img);
+    createCanvas(window.innerWidth, window.innerHeight);
+    background(41, 47, 54);
+    for (let i = 0; i < particleCount; i++) {
+        let p = new Particle();
+        particles.push(p);
     }
-  }
-
-
-  for (let i = 0; i < 400; i++) {
-    let x = random(width);
-    let y = random(height);
-    let design = random(textures);
-    snow.push(new Snowflake(x, y, design));
-  }
-
-
-
-
-
 }
 
+
+
 function draw() {
-  background(0);
-  //snow.push(new Snowflake());
+    background(41, 47, 54);
+    //orange
+    //const particleColor = color(242, 147, 24);
+    const particleColor = color(127);
+    const lineColor = color(255, 255, 255);
+    
 
-  zOff += 0.1;
+    for (let i = 0; i < particles.length; i++) {
+        noStroke();
+        fill(particleColor);
+        particles[i].draw();
+        particles[i].update();
+    }
 
-  for (flake of snow) {
-    let xOff = flake.pos.x / width;
-    let yOff = flake.pos.y / height;
-    let wAngle = noise(xOff, yOff, zOff) * TWO_PI;
-    let wind = p5.Vector.fromAngle(wAngle);
-    wind.mult(0.1);
+    for (let i = 0; i < particles.length - 1; i++) {
 
-    flake.applyForce(gravity);
-    flake.applyForce(wind);
-    flake.update();
-    flake.render();
-  }
+        let current = particles[i];
+        let affected = [];
 
-  // for (let i = snow.length - 1; i >= 0; i--) {
-  //   if (snow[i].offScreen()) {
-  //     snow.splice(i, 1);
-  //   }
-  // }
+        for (let j = 0; j < particles.length; j++) {
+            if (particles[j] != current) {
+                let distance = current.position.dist(particles[j].position);
+                if (distance <= maxDistance && affected.length <= maxConnections) {
+                    affected.push({
+                        'particle': particles[j],
+                        'distance': distance
+                    });
+                }
+            }
+        }
 
+        for (let a = 0; a < affected.length; a++) {
+            stroke(red(lineColor), green(lineColor), blue(lineColor), map(affected[a].distance, 0, maxDistance, 255, 0));
+            //smooth();
+            line(current.position.x, current.position.y, affected[a].particle.position.x, affected[a].particle.position.y);
+        }
+
+        
+    }
+}
+
+function Particle() {
+    this.position = createVector(random(0, window.innerWidth), random(0, window.innerHeight));
+    this.velocity = createVector(random(-1, 1), random(-1, 1));
+    this.velocity.normalize();
+    this.velocity.mult(random(minSpeed, maxSpeed));
+
+    this.update = function () {
+        this.position.add(this.velocity);
+        if (this.position.x < 0) this.position.x = window.innerWidth;
+        if (this.position.y < 0) this.position.y = window.innerHeight;
+        if (this.position.x > window.innerWidth) this.position.x = 0;
+        if (this.position.y > window.innerHeight) this.position.y = 0;
+    };
+
+    this.draw = function () {
+        //point(this.position.x, this.position.y);
+        
+        ellipse(this.position.x, this.position.y, 5, 5);
+    };
 }
